63651cbd8ebd4d9ff7c7830b83b7371b9f748e7f
lschuarcz <lschuarcz@gmail.com>
2023-12-11 15:38:56 -0300

    perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,32 @@ class _QuestionnaireAppState extends State<QuestionarioApp> {
     });
   }
 
+  List<String> pergunta = [
+    'Pergunta 0: Seu companheiro(a) já mudou de comportamento após pedir algo e não ser atendido ?',
+    'Pergunta 1: Pede para você se afastar de amigos ou familiares com argumentos de que não são boa influência pra você ?',
+    'Pergunta 2: Crítica ou tenta controlar suas fotos nas redes sociais, vigia seus seguidores ou quem você segue, e pede para bloquear ou excluir pessoas que o(a) desagradem?',
+    'Pergunta 3: Mente facilmente ?',
+    'Pergunta 4: Desqualifica ex companheiras(os)?',
+    'Pergunta 5: Quando confrontado(a) ou flagrado(a) mentindo, culpa você ou terceiros ?',
+    'Pergunta 6: É ciumento(a), vigia colegas de trabalho, critica suas roupas ou foto de perfil?',
+    'Pergunta 7: Exige livre acesso ao seu celular e às suas conversas privadas?',
+    'Pergunta 8: Seu companheiro(a) já teve ataques de fúria? Quebrou seu celular? Rasgou alguma roupa ou destruiu um presente?',
+    'Pergunta 9: Já utilizou palavras ofensivas durante uma discussão?',
+    'Pergunta 10: Seu companheiro(a) já ameaçou expor a intimidade de vocês ?',
+    'Pergunta 11: Já te empurrou ou segurou com força ?',
+    'Pergunta 12: Já puxou seu cabelo, te derrubou, te chutou ou te estapeou ?',
+    'Pergunta 13: Já te impediu de sair do carro ou de algum local durante uma discussão?',
+    'Pergunta 14: Já arremessou objetos contra você?',
+    'Pergunta 15: Já te ameaçou com uma faca, facão, pedaço de madeira ou similar?',
+    'Pergunta 16: Seu companheiro(a) já te ameaçou com arma de fogo ou mencionou comprar uma ?',
+    'Pergunta 17: Seu companheiro(a) já tentou machucar a si mesmo ou já falou em suicídio?',
+    'Pergunta 18: Tenta controlar como você gasta o seu dinheiro ?',
+    'Pergunta 19: Já forçou uma relação sexual ou te chantageou dizendo que é sua obrigação?',
+    'Pergunta 20: Seu companheiro(a) já falou ou insinuou que vai te matar?',
+
+    // Adicione as perguntas restantes aqui
+  ];
+
   // Function to build the widget
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -110,7 +136,7 @@ class _QuestionnaireAppState extends State<QuestionarioApp> {
               ),
         SizedBox(height: 30),
         Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text('Pergunta ${respostaAtual + 1}: Você concorda?'),
+          Text(pergunta[respostaAtual]),
           SizedBox(height: 30),
         ]),
         Row(
